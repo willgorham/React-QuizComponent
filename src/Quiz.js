f39a8d93ad5745f842edad58378a2cd7abf30b33
Will Gorham <willgorham@gmail.com>
2019-05-22 16:56:13 -0400

    Completed module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -21,13 +21,17 @@ class Quiz extends Component {
         });
     }
 
+    handleResetClick() {
+        this.setState({ quiz_position: 1 });
+    }
+
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
         return (
             <div>
                 {isQuizEnd 
-                    ? <QuizEnd />
+                    ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
                     : <QuizQuestion 
                         quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
                         showNextQuestionHandler={this.showNextQuestion.bind(this)}
